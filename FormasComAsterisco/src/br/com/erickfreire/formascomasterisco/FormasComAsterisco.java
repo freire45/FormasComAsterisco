8a00c670fd929372176590546fbf248b16073492
Erick Freire <erilefr@gmail.com>
2021-03-26 22:58:53 -0300

    FormasComAsterisco
    FormasComAsterisco

---
+++ FormasComAsterisco/src/br/com/erickfreire/formascomasterisco/FormasComAsterisco.java
@@ -0,0 +1,23 @@
+package br.com.erickfreire.formascomasterisco;
+
+
+/**
+ * Crie um programa em Java que desenhar um quadrado, um losango, uma seta e um circulo com asterisco
+ * @author Erick Freire
+ * @version 1 - Criado em 26-03-2021 as 22:54
+ */
+
+public class FormasComAsterisco {
+	public static void main(String[] args) {
+		System.out.printf("*********     ***      *         *    %n"
+				        + "*       *    *   *    ***       * *   %n"
+				        + "*       *   *     *  *****     *   *  %n"
+				        + "*       *   *     *    *      *     * %n"
+				        + "*       *   *     *    *     *       *%n"
+				        + "*       *   *     *    *      *     * %n"
+				        + "*       *   *     *    *       *   *  %n"
+				        + "*       *    *   *     *        * *   %n"
+				        + "*********     ***      *         *    %n");
+	}
+
+}
